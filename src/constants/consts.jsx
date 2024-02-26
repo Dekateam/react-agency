b507c6dcda41287a9f7f0e66ea29700861682227
javad <javadht1990a@gmail.com>
2024-02-26 23:29:04 +0330

    Add the constants file with instruction into the project

---
+++ src/constants/consts.jsx
@@ -0,0 +1,27 @@
+//Add all mapped constants here & export it to your component
+// Delete the Example part
+
+// Example
+
+// const services = [
+//     {
+//       title: "Front-End Developer",
+//       icon: web,
+//     },
+//     {
+//       title: "UI/UX Designer",
+//       icon: mobile,
+//     },
+//     {
+//       title: "WordPress Theme",
+//       icon: backend,
+//     },
+//     {
+//       title: "Responsive & Clean Code",
+//       icon: creator,
+//     },
+//   ];
+
+//   export { services }
+
+//===================================================
